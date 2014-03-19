36237099d6fd1528ce3f020977c450685f08fdba
Frode Petterson <frode.petterson@amendor.com>
2014-03-19 10:13:37 +0100

    HFJ-188: Fixed libraries resize support.

--- js/questionset.js
+++ js/questionset.js
@@ -155,8 +155,8 @@ H5P.QuestionSet = function (options, contentId) {
 
     // Trigger resize on question in case the size of the QS has changed.
     var instance = questionInstances[questionNumber];
-    if (instance.resize !== undefined) {
-      instance.resize();
+    if (instance.$ !== undefined) {
+      instance.$.trigger('resize');
     }
 
     // Update progress indicator
