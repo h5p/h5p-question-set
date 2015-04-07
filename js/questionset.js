3cab3010fa843c56be738cecd0e543d4644df781
Svein-Tore Griff With <s.t.g.with@gmail.com>
2015-04-07 19:16:44 +0200

    Update call to newRunnable to use update api

--- js/questionset.js
+++ js/questionset.js
@@ -123,7 +123,7 @@ H5P.QuestionSet = function (options, contentId) {
         enableSolutionsButton: params.override.overrideShowSolutionButton
       });
     }
-    var questionInstance = H5P.newRunnable(question, contentId, null, null, self);
+    var questionInstance = H5P.newRunnable(question, contentId, undefined, undefined, {parent: self});
     questionInstances.push(questionInstance);
     questionInstance.on('resize', function() {
       self.trigger('resize');
