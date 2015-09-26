198ae5fd3e28036421249c2135f715f4e0a72db8
Thomas Marstrander <thomasmars87@gmail.com>
2015-09-26 17:38:07 +0200

    Add 'finish' button as hidden, do not show it before all questions has been answered. [HFJ-1029]

--- js/questionset.js
+++ js/questionset.js
@@ -384,7 +384,7 @@ H5P.QuestionSet = function (options, contentId) {
         // Add finish question set button
         question.addButton('finish', params.texts.finishButton, function () {
           _displayEndGame();
-        });
+        }, false);
 
       } else {
 
